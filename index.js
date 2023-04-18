3cb5e6f9c57aa0321efcc6fe82c280df7f86987c
jadilovic <j.adilovic@gmail.com>
2023-04-19 00:15:42 +0200

    draft final

--- index.js
+++ index.js
@@ -124,6 +124,53 @@ const updatePage = () => {
 	main.appendChild(deleteModal);
 };
 
+function fromNow(date) {
+	var seconds = Math.floor((new Date() - date) / 1000);
+	var years = Math.floor(seconds / 31536000);
+	var months = Math.floor(seconds / 2592000);
+	var days = Math.floor(seconds / 86400);
+
+	if (days > 548) {
+		return years + ' years ago';
+	}
+	if (days >= 320 && days <= 547) {
+		return 'a year ago';
+	}
+	if (days >= 45 && days <= 319) {
+		return months + ' months ago';
+	}
+	if (days >= 26 && days <= 45) {
+		return 'a month ago';
+	}
+
+	var hours = Math.floor(seconds / 3600);
+
+	if (hours >= 36 && days <= 25) {
+		return days + ' days ago';
+	}
+	if (hours >= 22 && hours <= 35) {
+		return 'a day ago';
+	}
+
+	var minutes = Math.floor(seconds / 60);
+
+	if (minutes >= 90 && hours <= 21) {
+		return hours + ' hours ago';
+	}
+	if (minutes >= 45 && minutes <= 89) {
+		return 'an hour ago';
+	}
+	if (seconds >= 90 && minutes <= 44) {
+		return minutes + ' minutes ago';
+	}
+	if (seconds >= 45 && seconds <= 89) {
+		return 'a minute ago';
+	}
+	if (seconds >= 0 && seconds <= 45) {
+		return 'a few seconds ago';
+	}
+}
+
 const createComment = (
 	commentData,
 	index = 0,
@@ -184,7 +231,11 @@ const createComment = (
 	userName.textContent = commentData.user.username;
 
 	const timeStamp = document.createElement('span');
-	timeStamp.textContent = commentData.createdAt;
+	if (typeof commentData.createdAt == 'number') {
+		timeStamp.textContent = fromNow(commentData.createdAt);
+	} else {
+		timeStamp.textContent = commentData.createdAt;
+	}
 
 	userContainer.appendChild(userImg);
 	userContainer.appendChild(userName);
@@ -450,7 +501,7 @@ const newComment = (content) => {
 	const newComment = {
 		id: ++lastId,
 		content,
-		createdAt: new Date().toDateString(),
+		createdAt: new Date().getTime(),
 		score: 0,
 		user: {
 			...currentUser,
